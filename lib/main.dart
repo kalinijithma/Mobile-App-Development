2a1ee23e8251fa29e4915a92f0145f5fe5a988a1
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2024-01-26 18:44:19 +0530

    Added SingleChildScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -21,99 +21,123 @@ class MyApp extends StatelessWidget {
                         ),
                       backgroundColor: Colors.brown,
              actions: [
-                IconButton(onPressed: (){},icon: const Icon(Icons.menu))
+                IconButton(onPressed: (){}, icon: const Icon(Icons.shopping_cart_checkout)),
+                IconButton(onPressed: (){},icon: const Icon(Icons.menu),)  
           ],
           ),
 
          //body:const Icon(Icons.add),
          //Image.asset("assest/WELCOME.png"),
-          body: Column(
+          body: SingleChildScrollView(
+  child: Column(
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Container(
+        height: 215,
+        width: double.infinity,
+        child: Image(   
+          image: AssetImage("assest/WELCOME.png"),
+          fit: BoxFit.cover, 
+        ),
+      ),
 
-           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                height: 215,
-                width: double.infinity,
-                child: Image(   
-                   image: AssetImage("assest/WELCOME.png"),  //how to add a image
-                   fit: BoxFit.cover, 
-                 ),
-              ),
+      /* Container(
+        height: 100,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.brown,
+        ),
+      ), */
 
-             /* Container(
-                height: 100,
-                width: double.infinity,  // how to add a colour raw
-                decoration: BoxDecoration(
-                  color: Colors.brown,
+      
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 160, height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade50, borderRadius:BorderRadius.circular(30)
+            ),
+            child: Center(
+              child: Text(
+                "Neckless", 
+                textAlign: TextAlign.center, 
+                style: TextStyle(
+                  fontSize: 20, 
+                  fontWeight: FontWeight.w500,
                 ),
-              ),*/
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 160, height: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.brown.shade50, borderRadius:BorderRadius.circular(30)
-                    ),
-
-                    child: Center(
-                        child: Text("Neckless", 
-                        textAlign: TextAlign.center, 
-                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500, // Change the font size here as desired
-                          ),
-                        ),
-                       ),
-                    ),
-                  
-                    Container(
-                    width: 160, height: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.brown.shade100, borderRadius:BorderRadius.circular(30)
-                    ),
-                    ),
-                ],
               ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 160, height: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.brown.shade200, borderRadius:BorderRadius.circular(30)
-                    ),
-                    ),
-
-                    Container(
-                    width: 160, height: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.brown.shade300, borderRadius:BorderRadius.circular(30)
-                    ),
-                    ),
-                ],
-              ),
-
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                    width: 160, height: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.brown.shade400, borderRadius:BorderRadius.circular(30)
-                    ),
-                    ),
-
-                    Container(
-                    width: 160, height: 150,
-                    decoration: BoxDecoration(
-                      color: Colors.brown.shade500, borderRadius:BorderRadius.circular(30)
-                    ),
-                    ),
-                ],
-              ),
-            ],
+            ),
           ),
+          
+          Container(
+            width: 160, height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade100, borderRadius:BorderRadius.circular(30)
+            ),
+          ),
+        ],
+      ),
+
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 160, height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade200, borderRadius:BorderRadius.circular(30)
+            ),
+          ),
+
+          Container(
+            width: 160, height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade300, borderRadius:BorderRadius.circular(30)
+            ),
+          ),
+        ],
+      ),
+
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 160, height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade400, borderRadius:BorderRadius.circular(30)
+            ),
+          ),
+
+          Container(
+            width: 160, height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade500, borderRadius:BorderRadius.circular(30)
+            ),
+          ),
+        ],
+      ),
+
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            width: 160, height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade600, borderRadius:BorderRadius.circular(30)
+            ),
+          ),
+
+          Container(
+            width: 160, height: 150,
+            decoration: BoxDecoration(
+              color: Colors.brown.shade700, borderRadius:BorderRadius.circular(30)
+            ),
+          ),
+        ],
+      ),
+    ],
+  ),
+)
         ),
     );
   }
