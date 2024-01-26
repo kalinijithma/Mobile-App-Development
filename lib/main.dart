e1ff824a0e7886abeebf9e5653cab615d0536ad6
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2024-01-26 09:52:01 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,13 +14,26 @@ class MyApp extends StatelessWidget {
         home: Scaffold(
           appBar:AppBar(
             title:const Text("E L E G E N T"),
-            backgroundColor: Colors.black,
-          ),
-
-         //body:const Icon(Icons.add),
-          body: Image.asset("assest/WELCOME.png"),
-    
+            backgroundColor: Colors.brown,
             
+          ),
+         //body:const Icon(Icons.add),
+         //Image.asset("assest/WELCOME.png"),
+          body: Center(
+          child: Container(
+            height: 150,
+            width: 400,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(50),
+              color: Colors.brown, // Optional: Set container background color
+            ),
+          ),
+          ),
+          child: Container(Image.asset(
+              "assest/WELCOME.png", // Replace with your actual image path
+              height: 150,
+              width: 400,),
+            ),
         ),
     );
   }
