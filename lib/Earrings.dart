08e74d7d373c8765e3323c38c31dd29dd5f01b45
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2024-01-28 11:42:53 +0530

    .

--- lib/Earrings.dart
+++ lib/Earrings.dart
@@ -43,7 +43,7 @@ class _EarringsState extends State<Earrings> {
                   height: 215,
                   width: double.infinity,
                   child: const Image(   
-                    image: AssetImage("assest/earring.png"),
+                    image: AssetImage("assest/abc.png"),
                     fit: BoxFit.cover, 
                   ),
                 ),
